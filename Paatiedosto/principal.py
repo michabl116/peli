0acac8ddcd9ec9b4fbc7ed8ae0dd8d32bc96d170
michabl116 <michabl@metropolia.fi>
2024-10-08 10:03:03 +0300

    peli projekti

--- Paatiedosto/principal.py
+++ Paatiedosto/principal.py
@@ -1,9 +1,29 @@
 import subprocess
-"""0=======================0    
-   0      LENTO PELI       0
+
+from Paatiedosto import text_fuktion
+print("""
+    
+   0=======================0    
+   0      LENTO PELIT      0
    0                       0
-   0=======================0 """
-nimi= input("ana nimi:")
+   0=======================0 """)
+
+print()
+
+texti="""TERVETULOA PELEIMME!
+         SINULLA ON KOLME PELIä, JOISTA VALITTAA
+         PITÄÄ HAUSKAA!
+           
+           
+           """
+
+text_fuktion.print_with_delay(texti)
+print()
+print("TEKIJAT")
+print()
+print("ARTEM\nMATIAS\nFARHAM\nMICAHEL")
+print()
+nimi= input("ANNA PELI NIMI:")
 if nimi == "peli1":
     subprocess.run(["python", "alkupeli.py"])
 
